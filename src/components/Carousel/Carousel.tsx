8eb73634ba533b7aba0d647da44ae41bb699cd93
Mehmet Aras <arasmehmet07@gmail.com>
2024-05-07 00:25:58 +0300

    feat: basic logic and working example

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,67 @@
-export const Carousel = () => {
+import { FC, MouseEventHandler, useCallback, useEffect, useState } from "react";
+import "./Carousel.css";
+
+type CarouselProps = {
+  data: Record<string, string | number>[];
+  onChangeList: MouseEventHandler<HTMLButtonElement>
+};
+
+const Carousel: FC<CarouselProps> = ({ data, onChangeList }) => {
+  const [pics, setPics] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (data?.length !== 0) {
+      setPics(data);
+    }
+  }, [data]);
+
+  const onNext = useCallback(() => {
+    if (index === pics.length - 1) {
+      return setIndex(0);
+    }
+    return setIndex((prev) => prev + 1);
+  }, [index, pics]);
+
+  const onBack = useCallback(() => {
+    if (index === 0) {
+      return setIndex(pics.length - 1);
+    }
+    return setIndex((prev) => prev - 1);
+  }, [index, pics]);
+
   return (
-    <div>Carousel</div>
-  )
-}
+    <>
+      {data?.length && (
+        <div className="container">
+          <div className="image-wrapper">
+            {data.map((pic, i) => {
+              return (
+                <img
+                  style={{display: index === i ? 'block': 'none' }}
+                  className="image"
+                  src={`${pic?.download_url}`}
+                  alt={`${pic?.author}`}
+                />
+              );
+            })}
+            <span>
+              {index + 1} / {data?.length}
+            </span>
+          </div>
+          <div className="button-group">
+            <button className="back" onClick={onBack}>
+              Back
+            </button>
+            
+            <button className="next" onClick={onNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Carousel;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import Carousel from "../components/Carousel/Carousel";
+import { useCallback } from "react";
+import { throttle } from "lodash";
+
 export const MainPage = () => {
-  return <div className="main">
-    
-  </div>;
+  const { isPending, error, data, refetch } = useQuery({
+    queryKey: ["pics"],
+    queryFn: () =>
+      fetch(`https://picsum.photos/v2/list?limit=10&page=${random}`).then(
+        (res) => res.json()
+      ),
+  });
+
+  const onChangeList = useCallback(() => {
+    throttle(() => {}, 2000);
+  }, []);
+
+  return (
+    <div className="main">
+      <Carousel data={data} onChangeList={onChangeList} />
+    </div>
+  );
 };
