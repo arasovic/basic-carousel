ff9d69061883e807a0531c48418a2a872e742dd8
Mehmet Aras <arasmehmet07@gmail.com>
2024-05-06 19:03:23 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+import './App.css'
+import { MainPage } from './pages/MainPage'
+
+const queryClient = new QueryClient()
+
+function App() {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainPage/>
+    </QueryClientProvider>
+  )
+}
+
+export default App
